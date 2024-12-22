e5fc083436fc7d72f4d0af2b47dddcf1f861423a
Caleb Magruder <ccmagruder@gmail.com>
2024-12-22 14:49:55 -0600

    added : lspconfig clangd

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,8 @@ return {
   {
     'neovim/nvim-lspconfig',
     config = function()
-      require('lspconfig').ccls.setup({})
+      -- require('lspconfig').ccls.setup({})
+      require('lspconfig').clangd.setup({})
       require('lspconfig').pyright.setup({})
       require('lspconfig').lua_ls.setup({
         settings = {
