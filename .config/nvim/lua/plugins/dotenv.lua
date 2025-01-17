20c52b4f6b76e88bb8916e26f7a22aeac1d9aea4
Caleb Magruder <ccmagruder@gmail.com>
2025-01-16 19:54:52 -0600

    added : .env file reread on buffer write

--- .config/nvim/lua/plugins/dotenv.lua
+++ .config/nvim/lua/plugins/dotenv.lua
@@ -3,6 +3,7 @@ return {
     'ellisonleao/dotenv.nvim',
     config = function()
       require('dotenv').setup({
+        event = "BufWritePost",
         enable_on_load = true,
         verbose = false,
       })
