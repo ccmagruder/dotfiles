4d31f36403406e4d9fe215be89992b2c0662ade8
Caleb Magruder <ccmagruder@gmail.com>
2025-04-22 20:19:36 -0500

    adding : lualine filename relative path

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -12,12 +12,11 @@ return {
         sections = {
           lualine_a = {'mode'},
           lualine_b = {'diff', 'diagnostics'},
-          lualine_c = {'filename'},
+          lualine_c = { {'filename', path = 1} },
           lualine_x = {'filetype'},
           lualine_y = {'progress'},
           lualine_z = {'location'},
         },
-        
         tabline = {
           lualine_a = {'buffers'},
           lualine_b = {},
