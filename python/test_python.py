4eb0e58fc871ea99d593b06ab5500204b3e47b3e
Caleb Magruder <ccmagruder@gmail.com>
2025-08-24 19:33:25 -0500

    added : python dev env in python/

---
+++ python/test_python.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_python_pass():
+    x: int = 2
+    assert x == 2
+
+def test_python_fail():
+    x: int = 2
+    assert x == 2
+
+
+@pytest.mark.parametrize(("lhs","rhs"), [(3, 4), (5, 7)]) 
+def test_parameterized(lhs, rhs):
+    assert lhs + 1 == rhs
+
+
