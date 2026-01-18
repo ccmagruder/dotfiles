d2b48340a7fd66e89d96b03ed170536c1da5776a
Caleb Magruder <ccmagruder@gmail.com>
2026-01-18 07:10:03 -0600

    migrated : pyright -> mypy

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,19 +2,35 @@ return {
   {
     'neovim/nvim-lspconfig',
     config = function()
-      -- require('lspconfig').ccls.setup({})
-      require('lspconfig').clangd.setup({})
-      require('lspconfig').pyright.setup({})
-      require('lspconfig').lua_ls.setup({
+      vim.lsp.config('lua_ls',
+        {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { 'vim' },
+              },
+            },
+          },
+        }
+      )
+      vim.lsp.config('yamlls', {settings = {}})
+      vim.lsp.config('pylsp', {
         settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
+          pylsp = {
+            plugins = {
+              pylsp_mypy = {
+                enabled = true,
+                -- Does not work if both live_mode and dmypy are true
+                live_mode = false, -- Check as you type
+                dmypy = true, -- Use the mypy daemon for speed
+              },
+              pyflakes = { enabled = false },
+              pycodestyle = { enabled = false },
             },
           },
         },
       })
-      require('lspconfig').yamlls.setup({})
+      vim.lsp.enable('pylsp')
     end
   },
 }
--- python/test_python.py
+++ python/test_python.py
@@ -1,18 +1,17 @@
 import pytest
 
 
-def test_python_pass():
+def test_python_pass() -> None:
     x: int = 2
     assert x == 2
 
-
-def test_python_fail():
+def test_python_fail() -> None:
     x: int = 2
     assert x == 3
 
 
 @pytest.mark.parametrize(("lhs","rhs"), [(3, 4), (5, 7)]) 
-def test_parameterized(lhs, rhs):
+def test_parameterized(lhs, rhs) -> None:
     assert lhs + 1 == rhs
 
 
