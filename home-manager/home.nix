bfa1099fcf5c9354a75b54f1e249798fd1c92b96
ccmagruder <ccmagruder@gmail.com>
2026-01-23 16:56:02 -0600

    added : ide tmux setup alias

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -74,8 +74,8 @@ in
     shellAliases = {
       ll = "ls -lah";
       gs = "git status";
-      config = "nvim /etc/nixos/configuration.nix";
       update = "sudo nixos-rebuild test";
+      ide = "tmux -u new-session -n nixos -c ~/nixos \"nvim -c NvimTreeToggle\" \\; split-window -v \\; new-window -n dotfiles -c ~/dotfiles \"nvim -c NvimTreeToggle\" \\; split-window -v -c ~/dotfiles";
     };
     enableCompletion = true;
     oh-my-zsh = {
