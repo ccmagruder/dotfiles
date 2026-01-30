50eb2f4a1b716bbe767fba43e2069302960ef73f
ccmagruder <ccmagruder@gmail.com>
2026-01-29 20:56:18 -0600

    added : nvim-tree indent markers

--- home-manager/ide/nixvim/nvim-tree.nix
+++ home-manager/ide/nixvim/nvim-tree.nix
@@ -1,6 +1,9 @@
 { ... }:
 {
-  programs.nixvim.plugins.nvim-tree.enable = true;
+  programs.nixvim.plugins.nvim-tree = {
+    enable = true;
+    settings.renderer.indent_markers.enable=true;
+  };
   programs.nixvim.keymaps = [
     {
       mode = "n";
