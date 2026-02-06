191182fa2d2eb371b8bfa551d3a21a47ebcf9fb4
ccmagruder <ccmagruder@gmail.com>
2026-02-06 05:43:06 -0600

    added : nix develop --command nvim ... to .bat.json

--- home-manager/ide/nixvim/bat.nvim/lua/bat/commands.lua
+++ home-manager/ide/nixvim/bat.nvim/lua/bat/commands.lua
@@ -1,9 +1,12 @@
 local M = {}
 
+
 function M.setup(options)
   vim.api.nvim_create_user_command("Bat", function(opts)
+    local bat = require("bat")
     local arg = opts.args
-    cmds = require("bat").read_json_config(options.path)
+    cmds = bat.read_json_config(options.path)
+    bat.run(cmds[arg])
   end, { nargs = 1 })
 end
 
