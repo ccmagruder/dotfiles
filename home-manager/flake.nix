5f4b66613fc88cb38879af7cf4cf93e2bd5a4b4a
ccmagruder <ccmagruder@gmail.com>
2026-03-02 15:56:59 -0600

    added : vm host

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -23,10 +23,16 @@
   outputs = inputs@{ flake-parts, nixvim, sops-nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [inputs.home-manager.flakeModules.home-manager ];
-      systems = [ "x86_64-linux" ];
-      flake.homeConfigurations."remote" =
+      systems = [ "aarch64-linux" "x86_64-linux" ];
+      flake.homeConfigurations."remote@x86" =
         inputs.home-manager.lib.homeManagerConfiguration {
           pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+          modules = [ ./home.nix ./ide sops-nix.homeManagerModules.sops ];
+          extraSpecialArgs = { inherit inputs; };
+        };
+      flake.homeConfigurations."remote@arm" =
+        inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = inputs.nixpkgs.legacyPackages."aarch64-linux";
           modules = [ ./home.nix ./ide sops-nix.homeManagerModules.sops ];
           extraSpecialArgs = { inherit inputs; };
         };
