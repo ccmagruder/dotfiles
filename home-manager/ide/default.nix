75a3dc25aab92dbd5f15633232968387fa15b25e
Caleb Magruder <caleb.magruder@simplerose.com>
2026-07-09 09:09:56 -0500

    add: ide/nixvim/nixvim.nix

--- home-manager/ide/default.nix
+++ home-manager/ide/default.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ inputs, ... }:
 {
   imports = [
+    inputs.nixvim.homeModules.nixvim
     ./claude.nix
     ./git.nix
     ./home-manager.nix
---
+++ home-manager/ide/nixvim/nixvim.nix
@@ -0,0 +1,77 @@
+{ ... }:
+{
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.nixvim = {
+    enable = true;
+
+    # Vim Options
+    opts = {
+      number = true;
+      tabstop = 2;
+      expandtab = true;
+      shiftwidth = 2;
+      autoread = true;
+      winhighlight = "Normal:Normal,NormalNC:NormalNC";
+      ttimeoutlen = 10;
+      timeoutlen = 500;
+    };
+
+    # Color Theme
+    colorschemes.nord.enable = true;
+    extraConfigLua = ''
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" }) -- background black
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1a1a1a" }) -- dimmed background for inactive windows
+      vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#000000" }) -- nvim-tree active background
+      vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "#1a1a1a" }) -- nvim-tree inactive background (dimmed)
+
+      -- Dim all neovim windows when losing focus to another tmux pane
+      local focus_group = vim.api.nvim_create_augroup("TmuxFocusDim", { clear = true })
+
+      vim.api.nvim_create_autocmd("FocusLost", {
+        group = focus_group,
+        callback = function()
+          vim.api.nvim_set_hl(0, "Normal", { bg = "#1a1a1a" })
+          vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#1a1a1a" })
+          -- Dim lualine by setting all sections to use inactive colors
+          vim.opt.winhl:append("StatusLine:StatusLineNC")
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("FocusGained", {
+        group = focus_group,
+        callback = function()
+          vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+          vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#000000" })
+          -- Restore lualine active colors
+          vim.opt.winhl:remove("StatusLine:StatusLineNC")
+        end,
+      })
+    '';
+
+    # Keyboard shortcuts
+    globals.mapleader = " ";
+    globals.maplocalleader = " ";
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader><Tab>";
+        action = "<cmd>bn<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>s";
+        action = "<cmd>w<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>q";
+        action = "<cmd>qa<CR>";
+      }
+    ];
+
+    clipboard.register = "unnamedplus";
+  };
+}
