7c68bc656644ed05371898f1cfe7c2779afab9e2
Caleb Magruder <caleb.magruder@simplerose.com>
2026-07-10 11:41:13 -0500

    add: vector includes ccache in path

--- home-manager/home-vector.nix
+++ home-manager/home-vector.nix
@@ -2,7 +2,10 @@
 {
   home.username = "caleb";
   home.homeDirectory = "/home/caleb";
-  programs.zsh.envExtra = "export STARSHIP_HOST_ALIAS=vector";
+  programs.zsh.envExtra = ''
+    export STARSHIP_HOST_ALIAS=vector
+    export PATH="/usr/lib/ccache:$PATH"
+  '';
 
   programs.git.settings.user.email = "caleb.magruder@simplerose.com";
 }
